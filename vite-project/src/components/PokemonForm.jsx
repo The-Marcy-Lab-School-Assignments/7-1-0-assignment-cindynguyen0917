d8dcaa13ca80bf38ccf3caf063f89f51a92a0568
Cindy Nguyen <cnguyen5138@gmail.com>
2024-08-15 11:51:52 -0400

    done

--- vite-project/src/components/PokemonForm.jsx
+++ vite-project/src/components/PokemonForm.jsx
@@ -1,24 +1,63 @@
+import { useContext, useState } from "react"
+import PokemonContext from "../context/PokemonContext"
+
 const PokemonForm = () => {
+    const { setAllPokemon } = useContext(PokemonContext)
+    const [name, setName] = useState('')
+    const [hp, setHp] = useState('')
+    const [frontSprite, setFrontSprite] = useState('');
+    const [backSprite, setBackSprite] = useState('');
+
+    const handleSumbit = async (event) => {
+        event.preventDefault();
+        const newPokemon = {
+            name,
+            hp: parseInt(hp, 10),
+            front: frontSprite,
+            back: backSprite
+
+        }
+        try {
+            const exampleOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newPokemon),
+            };
+            const url = "http://localhost:4000/pokemon";
+            const response = await fetch(url, exampleOptions);
+            const data = await response.json();
+            setAllPokemon(currentPokemon => [...currentPokemon, data]);
+        }
+        catch (error) {
+            setError(error)
+        }
+        setName('');
+        setHp('');
+        setFrontSprite('');
+        setBackSprite('');
+    }
     return (
         <div>
             <h3>Add a Pokemon!</h3>
-            <form className="ui form">
+            <form className="ui form" onSubmit={handleSumbit}>
                 <div className="four fields" widths="equal">
                     <div className="field ui fluid">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="name" placeholder="Name" value={name} onChange={(event) => setName(event.target.value)} />
                     </div>
                     <div className="field ui fluid">
                         <label>HP</label>
-                        <input type="text" name="hp" placeholder="HP" />
+                        <input type="text" name="hp" placeholder="HP" value={hp} onChange={(event) => setHp(event.target.value)} />
                     </div>
                     <div className="field ui fluid">
                         <label>Front Image URL</label>
-                        <input type="text" name="front" placeholder="url" />
+                        <input type="text" name="front" placeholder="url" value={frontSprite} onChange={(event) => setFrontSprite(event.target.value)} />
                     </div>
                     <div className="field ui fluid">
                         <label>Back Image URL</label>
-                        <input type="text" name="back" placeholder="url" />
+                        <input type="text" name="back" placeholder="url" value={backSprite} onChange={(event) => setBackSprite(event.target.value)} />
                     </div>
                 </div>
                 <button className="ui button" type="submit">Submit</button>
@@ -26,5 +65,6 @@ const PokemonForm = () => {
         </div>
     )
 }
+
 
 export default PokemonForm
